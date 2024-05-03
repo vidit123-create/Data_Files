2ff2afef760a2c044f30ca5cb572ad752fb52592
Vidit <viditgoyal775@gmai.com>
2024-05-04 01:26:39 +0530

    Break_N_Continue_Keyword_Concept

--- Question on Loops.py
+++ Question on Loops.py
@@ -1,8 +1,24 @@
 """Show the concept of break keyword"""
-chances=1
-while(chances<=3):
-    start, end = int(input("Starting Value :")), int(input("Ending Value :"))
-    num = int(input("Enter any value :"))
+"""chances=1"""
+print("..................Program Starts")
+list_1,Len=list(),int(input("Length of Given List :"))
+for i in range(0,Len):
+    list_1=list_1+[int(input("Enter any element :"))]
+print(f"Input List :{list_1}")
+while(True):
+    search_element = int(input("Element to be Searched :"))
+    """
+    for num in list_1[:len(list_1):1]:
+        if num==search_element:
+            print(f"Search Completed :{num}")
+    """
+    if search_element in list_1[:len(list_1):1]:
+        print(f"Search Completed and number is {search_element}")
+        break
+    else:
+        print("Loop Continues...")
+        continue
+    """
     while(start<=end):
         if start==num:
             print("Hello World")
@@ -11,3 +27,5 @@ while(chances<=3):
     else:
         print("Inner Loop Terminated...")
     chances+=1
+    """
+print(".......................Program Ends")
