d5a9de4b4fd5109ca9a6ea4e871cee703b6ec340
Vidit <viditgoyal775@gmai.com>
2024-05-03 15:07:33 +0530

    Break_Keyword_Concept

---
+++ Question on Loops.py
@@ -0,0 +1,13 @@
+"""Show the concept of break keyword"""
+chances=1
+while(chances<=3):
+    start,end=int(input("Starting Value :")),int(input("Ending Value :"))
+    num=int(input("Enter any value :"))
+    while(start<=end):
+        if start==num:
+            print("Hello World")
+            break
+        start+=1
+    else:
+        print("Inner Loop Terminated...")
+    chances+=1
