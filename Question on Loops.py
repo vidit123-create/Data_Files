b295857bdd76ed24ca12adaa8d7c9bddcf932037
Vidit <viditgoyal775@gmai.com>
2024-05-04 01:15:04 +0530

    Break_N_Continue_Keyword_Concept

--- Question on Loops.py
+++ Question on Loops.py
@@ -1,8 +1,8 @@
 """Show the concept of break keyword"""
 chances=1
 while(chances<=3):
-    start,end=int(input("Starting Value :")),int(input("Ending Value :"))
-    num=int(input("Enter any value :"))
+    start, end = int(input("Starting Value :")), int(input("Ending Value :"))
+    num = int(input("Enter any value :"))
     while(start<=end):
         if start==num:
             print("Hello World")
