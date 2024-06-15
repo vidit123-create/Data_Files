4c408f274ff076e1fe909dcda006dfb077c05593
Vidit <viditgoyal775@gmai.com>
2024-06-15 09:24:47 +0530

    Concept_of_File_Handling

---
+++ File_Handling_Concept.py
@@ -0,0 +1,21 @@
+"""
+Concept of File Handling...
+"""
+# f=open('FirstFile.txt','x')
+# f.close()
+# while(True):
+#     def add_values(list_1):
+#         sum=0
+#         for i in list_1[::]:
+#             sum=sum+i
+#         return sum
+#     Len=input("Total Length :")
+#     if Len.isnumeric()==True:
+#         list_2=list()
+#         for i in range(0,int(Len)):
+#             list_2.append(int(input("Element to be Inserted :")))
+#         print(f"Sum of all the values in a given list :{add_values(list_1=list_2)}")
+#     else:
+#         break
+f=open('FirstFile.txt','a')
+#f=open('FirstFile.txt','r')
